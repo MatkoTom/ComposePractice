193eee8ea9476a11c4e14209f546ba5a8cc20b86
MatkoTom <matko@clover.studio>
2023-10-05 14:28:08 +0200

    Picker item can now be pressed on whole container, and ripple displays correctly.

--- app/src/rotamap/java/PickerSelector.kt
+++ app/src/rotamap/java/PickerSelector.kt
@@ -5,19 +5,21 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,31 +36,30 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun PickerSelector(selected: Int) {
-    Surface {
-        var expanded by remember { mutableStateOf(false) }
-        var selectedItem by remember {
-            mutableStateOf(selected)
-        }
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember {
+        mutableStateOf(selected)
+    }
 
-        Column(
-            modifier = Modifier
-                .animateContentSize(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioMediumBouncy,
-                        stiffness = Spring.StiffnessLow
-                    )
+    Column(
+        modifier = Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
                 )
-                .clickable { expanded = !expanded }
-        ) {
-            PickerTitleBar(
-                text = R.string.label,
-                selectedItem = selectedItem,
             )
-            if (expanded) {
-                PickerContent(topLabel = selectedItem,
-                    onChanged = { selectedItem = it }
-                )
-            }
+            .clickable { expanded = !expanded }
+            .padding(16.dp)
+    ) {
+        PickerTitleBar(
+            text = R.string.label,
+            selectedItem = selectedItem,
+        )
+        if (expanded) {
+            PickerContent(topLabel = selectedItem,
+                onChanged = { selectedItem = it }
+            )
         }
     }
 }
@@ -109,7 +111,8 @@ fun PickerContent(topLabel: Int, onChanged: (Int) -> Unit) {
 
     LazyColumn(
         modifier = Modifier.padding(top = 32.dp, bottom = 32.dp, start = 8.dp, end = 8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         items(pickerItems) { item ->
             PickerContentItem(
@@ -145,11 +148,12 @@ fun PickerContentItem(
         textAlign = TextAlign.Center,
         modifier = Modifier
             .background(backgroundValue)
-            .padding(12.dp)
             .fillMaxWidth()
+            .height(40.dp)
             .clickable {
                 onChanged(text)
             }
+            .wrapContentSize()
     )
 }
 
